a1afec7266e9cf3940dacf0548dba6c07c7f6b6c
Nuubles <matti.mikael.turpeinen@gmail.com>
2019-02-23 01:30:16 +0200

    Made sample classes

--- Foxet/src/net/karanteeni/foxet/movement/MoveManager.java
+++ Foxet/src/net/karanteeni/foxet/movement/MoveManager.java
@@ -1,22 +1,63 @@
 package net.karanteeni.foxet.movement;
 
+import java.util.Deque;
 import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 import net.karanteeni.core.timers.KaranteeniTimer;
+import net.karanteeni.foxet.Foxet;
 
-public class MoveManager implements KaranteeniTimer 
+/**
+ * Manages the movement data collected from players
+ * @author Nuubles
+ *
+ */
+public class MoveManager 
+{
+	/** How ofter player movement is polled in ticks */
+	private final int pollfreq;
+	
+	/**
+	 * Initializes the movemanager and loads config
+	 */
+	public MoveManager()
+	{
+		//Check if movement poll frequency is set
+		Foxet plugin = Foxet.getPlugin(Foxet.class);
+		if(!plugin.getConfig().isSet("PollFrequency"))
+		{
+			plugin.getConfig().set("PollFrequency", "10");
+			plugin.saveConfig();
+		}
+		
+		pollfreq = plugin.getConfig().getInt("PollFrequency");
+	}
+}
+
+/**
+ * This class is used to poll the players and their movements onto a hashmap
+ * @author Nuubles
+ */
+final class Poller implements KaranteeniTimer 
 {
 	/** Locations of player over time */
-	private HashMap<UUID,List<Location>> locations = new HashMap<UUID,List<Location>>();
+	private HashMap<UUID,Deque<Location>> locations = new HashMap<UUID,Deque<Location>>();
 	
 	@Override
 	public void runTimer() {
-		// TODO Auto-generated method stub
-		
+		//Loop all players
+		for(Player player : Bukkit.getOnlinePlayers())
+		{
+			//Check that player is not an npc
+			if(player.getMetadata("NPC") != null && !player.getMetadata("NPC").isEmpty())
+				continue;
+			
+			
+		}
 	}
 
 	@Override
